b2fc0fa68f0bd47493eeb4bfaf935dbe3c14e974
Mahdiar <mahdyarvaez@gmail.com>
2025-02-21 11:35:46 -0800

    user model added

---
+++ Models/userMd.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+const userSchema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match:/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+    },
+    role:{
+    type:String,
+    default:'user',
+    enum:['user','admin']
+    }
+},{timestamps:true})
+const User=mongoose.model('User',userSchema)
+export default User
